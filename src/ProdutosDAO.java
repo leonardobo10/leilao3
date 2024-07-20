12dbb73680443dae2389f5d47cc11227b4113797
Leonardo Bernardino <leonardobernardinodeandrade@gmail.com>
2024-07-19 21:35:17 -0300

    Função de Listagem de Vendas

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -14,6 +14,7 @@ import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class ProdutosDAO {
@@ -83,6 +84,32 @@ public class ProdutosDAO {
         }
         catch(SQLException ex) {
             JOptionPane.showMessageDialog(null,"Erro ao vender produto");
+        }
+    
+    }
+       public List<ProdutosDTO> listarProdutosVendidos(){
+        List<ProdutosDTO> lista = new ArrayList<>();
+        conectaDAO conecta = new conectaDAO();
+        conn = conecta.connectDB();
+        PreparedStatement ps=null;
+        try{
+            ps =conn.prepareStatement("SELECT*FROM produtos WHERE status = ?");
+            ps.setString(1, "vendido");
+            resultset=ps.executeQuery();
+            while(resultset.next()){
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+                lista.add(produto);
+            
+        }
+            return lista;
+        
+    }
+        catch(SQLException ex){
+            return null;
         }
     
     }
