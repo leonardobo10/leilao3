d0421082ec26a373ac2e4eefd63f69abcdcebdae
Leonardo Bernardino <leonardobernardinodeandrade@gmail.com>
2024-07-19 21:10:16 -0300

    Função de Cadastro

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -12,6 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -23,11 +24,28 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
+   conectaDAO conecta = new conectaDAO();
+        conn = conecta.connectDB();
+        PreparedStatement ps = null;
+        try{
+            ps=conn.prepareStatement("INSERT INTO produtos (nome, valor, status) VALUES(?,?,?)");
+            ps.setString(1, produto.getNome());
+            ps.setInt(2, produto.getValor());
+            ps.setString(3, produto.getStatus());
+            ps.executeUpdate();
+            
+            conn.close();
+            ps.close();
+            
+        }
+        catch(SQLException ex) {
+            JOptionPane.showMessageDialog(null,"");
+        }
+        
+     
         
         
-        //conn = new conectaDAO().connectDB();
-        
-        
+    
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
